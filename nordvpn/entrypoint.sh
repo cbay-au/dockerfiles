59510feb2632e3d00cb44ce48517af9543d435b2
Leighton <linux@clucas.au>
2025-05-09 15:01:03 +0800

    changes from socrates on Fri May  9 03:01:03 PM AWST 2025

--- nordvpn/entrypoint.sh
+++ nordvpn/entrypoint.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
 
+# Source .env file
+. .env
+
+cat <<"EOF" >>.bashrc
+
+    alias vpns='nordvpn status'
+    alias vpnset='nordvpn settings'
+    alias vpnc='nordvpn connect Sweden'
+    alias vpndc='nordvpn connect Double_VPN'
+    alias vpno='s obfuscate on'
+    alias vpnuk='nordvpn c 2195'
+    alias vpnhk='nordvpn connect Hong_Kong'
+
+EOF
+
+. .bashrc
+
+echo "
+Will now automatically double connect
+     "
+
 # Login to NordVPN using the token
+
 nordvpn login --token $NORDVPN_TOKEN
+sleep 8
+nordvpn connect Double_VPN
+
+echo "
+The following alias can be used:-
+    alias vpns='nordvpn status'
+    alias vpnset='nordvpn settings'
+    alias vpnc='nordvpn connect Sweden'
+    alias vpndc='nordvpn connect Double_VPN'
+    alias vpno='s obfuscate on'
+    alias vpnuk='nordvpn c 2195'
+    alias vpnhk='nordvpn connect Hong_Kong'
+     "
+
+
 
 # Start an interactive bash session or any other command you need
 #exec /bin/bash
+
